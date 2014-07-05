785b3de71e87b38f44fc2a99258e099f5ca34cf0
Johannes Schickling <schickling.j@gmail.com>
2014-07-05 23:30:05 +0200

    more tests

--- test/index.js
+++ test/index.js
@@ -30,4 +30,48 @@ describe('gulp-webserver', function() {
 
   });
 
+  it('should work with custom port', function(done) {
+
+    var rootDir = new File({
+      path: __dirname + '/fixtures'
+    });
+
+    stream = webserver({
+      port: 1111
+    });
+
+    stream.write(rootDir);
+
+    request('http://localhost:1111')
+      .get('/')
+      .expect(200, /Hello World/)
+      .end(function(err) {
+        if (err) return done(err);
+        done(err);
+      });
+
+  });
+  
+  it('should work with custom host', function(done) {
+
+    var rootDir = new File({
+      path: __dirname + '/fixtures'
+    });
+
+    stream = webserver({
+      host: '0.0.0.0'
+    });
+
+    stream.write(rootDir);
+
+    request('http://0.0.0.0:8000')
+      .get('/')
+      .expect(200, /Hello World/)
+      .end(function(err) {
+        if (err) return done(err);
+        done(err);
+      });
+
+  });
+
 });
