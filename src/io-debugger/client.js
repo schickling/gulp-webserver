c749d5cf1d97c48ab3a54dfcd8e7d136e30db95d
Joel Chu <joelckchu@gmail.com>
2016-08-24 18:15:04 +0100

    change the client to hold a namespace
    this is for the ng-io-debugger to work

--- src/io-debugger/client.js
+++ src/io-debugger/client.js
@@ -1,28 +1,29 @@
 (function(window , navigator)
 {
 	/**
-	 * create a global $gulpWebserverIo to hold this connection  
+	 * create a global $gulpWebserverIo namespace to hold everything
 	 */
-    window.$gulpWebserverIo = io.connect('http://{host}:{port}{debuggerPath}');
+    window.$gulpWebserverIo = {};
 
-    window.$gulpWebserverIo.on('hello', function (msg) {
+    window.$gulpWebserverIo.server = io.connect('http://{host}:{port}{debuggerPath}');
+
+    window.$gulpWebserverIo.server.on('hello', function (msg) {
 		try {
         	console.log('debugger init connection: ' , msg);
 		} catch (e) {
-			// no console.log 
+			// no console.log
 		}
     });
 
     // core implementation
     window.addEventListener('error', function (e) {
-        
 		var stack = e.error.stack;
         var message = e.error.toString();
         if (stack) {
             message += '\n' + stack;
         }
-		
-        window.$gulpWebserverIo.emit('{eventName}' , {
+		window.$gulpWebserverIo.eventName = '{eventName}';
+        window.$gulpWebserverIo.server.emit(window.$gulpWebserverIo.eventName , {
 			msg: message,
 			browser: navigator.userAgent,
 			location: window.location.href
