5fbd46bb7364ded4ed9d27ef241dddf95551e7c9
joelchu <joelckchu@gmail.com>
2016-08-25 09:28:28 +0100

    update the client implementation

--- src/io-debugger/client.js
+++ src/io-debugger/client.js
@@ -1,28 +1,30 @@
 (function(window , navigator)
 {
+    'use strict';
 	/**
 	 * create a global $gulpWebserverIo namespace to hold everything
 	 */
-    window.$gulpWebserverIo = {};
-
-    window.$gulpWebserverIo.server = io.connect('http://{host}:{port}{debuggerPath}');
-
-    window.$gulpWebserverIo.server.on('hello', function (msg) {
+    window.$gulpWebserverIo = {
+        server: io.connect('http://{host}:{port}{debuggerPath}'),
+        eventName: '{eventName}'
+    };
+    // listen to connection
+    window.$gulpWebserverIo.server.on('hello', function (msg)
+    {
 		try {
         	console.log('debugger init connection: ' , msg);
 		} catch (e) {
 			// no console.log
 		}
     });
-
     // core implementation
-    window.addEventListener('error', function (e) {
+    window.addEventListener('error', function (e)
+    {
 		var stack = e.error.stack;
         var message = e.error.toString();
         if (stack) {
             message += '\n' + stack;
         }
-		window.$gulpWebserverIo.eventName = '{eventName}';
         window.$gulpWebserverIo.server.emit(window.$gulpWebserverIo.eventName , {
 			msg: message,
 			browser: navigator.userAgent,
