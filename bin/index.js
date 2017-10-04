f0a89a3d9142e1c13b348dab767ac3a00cba40cb
Joel Chu <joelckchu@gmail.com>
2017-10-04 16:56:41 +0100

    configure for dev

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,5 @@
-#!/usr/bin/env node
+/* eslint-disable */
+// #!/usr/bin/env node
 'use strict';
 /**
  * The cli
@@ -8,6 +9,7 @@ const chalk = require('chalk');
 const root = process.cwd();
 // Remember this need to run within gulp
 // so first thing to check is to see if I could call gulp first?
+/*
 try {
   const gulp = require('gulp');
 } catch (e) {
@@ -16,16 +18,27 @@ try {
   );
   process.exit();
 }
+*/
 // Check if there is a config file somewhere
-
 
 // Should I use argv here as well just to detect an option
 try {
   const argvs = process.argv;
+  const fileName = argvs.map( (arg , i) => {
+    if (arg ==='--config') {
+      return argvs[i+1];
+    }
+    return null;
+  }).reduce( (last , next) =>
+  {
+    if (next) {
+      console.log(next);
+    }
 
-  console.log(argvs);
+  } , null);
 
-} catch(e) {
+
+} catch (e) {
   console.log(
     chalk.red('No configuration found, create a wbio-config.json or use the --config option!')
   );
