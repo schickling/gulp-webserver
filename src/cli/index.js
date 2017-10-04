764a36c9ef8e0c6194bf1b69848ac3f1ca99248d
Joel Chu <joelckchu@gmail.com>
2017-10-04 16:06:13 +0100

    set up the cli

---
+++ src/cli/index.js
@@ -0,0 +1,7 @@
+/**
+ * the cli
+ */
+const webServerIO = require('../main.js');
+// check if there is a config file somewhere
+const root = process.cwd();
+// should I use argv here as well just to detect an option 
