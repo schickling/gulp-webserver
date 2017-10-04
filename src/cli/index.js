270efc172f3d68842e2efe7a2c5480e13f94ea62
Joel Chu <joelckchu@gmail.com>
2017-10-04 16:46:18 +0100

    move the src/cli to bin instead

--- src/cli/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * the cli
- */
-const webServerIO = require('../main.js');
-// check if there is a config file somewhere
-const root = process.cwd();
-// should I use argv here as well just to detect an option 
--- src/io-debugger/injection.js
+++ src/io-debugger/injection.js
@@ -9,7 +9,7 @@ module.exports = function (config, opt) {
   const ignore = opt.ignore || opt.excludeList || [/\.js$/, /\.css$/, /\.svg$/, /\.ico$/, /\.woff$/, /\.png$/, /\.jpg$/, /\.jpeg$/];
   const include = opt.include || [/.*/];
   const html = opt.html || _html;
-  // const tagName = 'head'; // or body
+  // Const tagName = 'head'; // or body
   // This is the important part to determine how the script get inserted into the page
   const rules = opt.rules || [{
     match: /<\/head>(?![\s\S]*<\/head>)/i, // @2017-07-13 change from head to body
